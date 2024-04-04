57e9036f7aa8fe89a17c8601aea2c51fe9c67972
DanielPinzonM <daniele.pinzon@udea.edu.co>
2024-04-03 23:04:28 -0500

    Se desarrollan la funcion de rotacion, la de estado de rotacion y eliminacion de matriz. Se añaden al modulo la funcion para imprimr matriz y para conocer la posicion respecto al centro de una casilla.

--- Matriz.cpp
+++ Matriz.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "Matriz.h"
 
 unsigned short int** CrearMatriz(unsigned short int* Dimension)
@@ -14,8 +15,8 @@ unsigned short int** CrearMatriz(unsigned short int* Dimension)
 
             for  (short int Columna=1; Columna<=(*Dimension); Columna++)
             {
-                Matriz[Fila-1][Columna-1] = Columna + ((Fila-1)*(*Dimension));
-                Matriz[(*Dimension)-Fila][(*Dimension)-Columna] = (*Dimension)-Columna+1 + ((((*Dimension)-Fila+1)-1)*(*Dimension))-1;
+                Matriz[Fila-1][Columna-1] = ObtenerValorCasilla(Fila, Columna, Dimension);
+                Matriz[(*Dimension)-Fila][(*Dimension)-Columna] = ObtenerValorCasilla( (*Dimension)-Fila+1, (*Dimension)-Columna+1, Dimension);
             }
         }
         else
@@ -24,8 +25,8 @@ unsigned short int** CrearMatriz(unsigned short int* Dimension)
 
             for (short int Columna=1; Columna<=((*Dimension)/2); Columna++)
             {
-                Matriz[Fila-1][Columna-1] = Columna + ((Fila-1)*(*Dimension));
-                Matriz[(*Dimension)-Fila][(*Dimension)-Columna] = (*Dimension)-Columna+1 + ((((*Dimension)-Fila+1)-1)*(*Dimension))-1;
+                Matriz[Fila-1][Columna-1] = ObtenerValorCasilla(Fila, Columna, Dimension);
+                Matriz[(*Dimension)-Fila][(*Dimension)-Columna] = ObtenerValorCasilla( (*Dimension)-Fila+1, (*Dimension)-Columna+1, Dimension);
             }
         }
     }
@@ -35,19 +36,58 @@ unsigned short int** CrearMatriz(unsigned short int* Dimension)
     return Matriz;
 }
 
-void RotarMatriz(unsigned short int** Matriz, unsigned short int* Dimension, short int CantRotaciones)
+void RotarMatriz(unsigned short int** Matriz, unsigned short int* Dimension, unsigned short int CantRotaciones)
 {
-
+    for (short int Fila=1; Fila<=((*Dimension)/2)+1; Fila++)
+    {
+        if (Fila < ((*Dimension)/2)+1)
+        {
+            for (short int Columna=1; Columna<=(*Dimension); Columna++)
+            {
+                Matriz[Fila-1][Columna-1] = ObtenerValorCasilla(TransformarFila(Fila, Columna, Dimension, &CantRotaciones), TransformarColumna(Fila, Columna, Dimension, &CantRotaciones), Dimension);
+                Matriz[(*Dimension)-Fila][(*Dimension)-Columna] = ObtenerValorCasilla(TransformarFila((*Dimension)-Fila+1, (*Dimension)-Columna+1, Dimension, &CantRotaciones), TransformarColumna((*Dimension)-Fila+1, (*Dimension)-Columna+1, Dimension, &CantRotaciones), Dimension);
+            }
+        }
+        else
+        {
+            for (short int Columna=1; Columna<=((*Dimension)/2); Columna++)
+            {
+                Matriz[Fila-1][Columna-1] = ObtenerValorCasilla(TransformarFila(Fila, Columna, Dimension, &CantRotaciones), TransformarColumna(Fila, Columna, Dimension, &CantRotaciones), Dimension);
+                Matriz[(*Dimension)-Fila][(*Dimension)-Columna] = ObtenerValorCasilla(TransformarFila((*Dimension)-Fila+1, (*Dimension)-Columna+1, Dimension, &CantRotaciones), TransformarColumna((*Dimension)-Fila+1, (*Dimension)-Columna+1, Dimension, &CantRotaciones), Dimension);
+            }
+        }
+    }
 }
 
 unsigned short int ObtenerEstadoRotacion(unsigned short int** Matriz, unsigned short int* Dimension)
 {
-
+    if (Matriz[0][0] == 1)
+    {
+        return 0;
+    }
+    else if (Matriz[0][0] == (*Dimension))
+    {
+        return 1;
+    }
+    else if (Matriz[0][0] == ((*Dimension)*(*Dimension))-1)
+    {
+        return 2;
+    }
+    else if (Matriz[0][0] == ((*Dimension)*(*Dimension))-(*Dimension))
+    {
+        return 3;
+    }
 }
+
 
 void EliminarMatriz(unsigned short int** Matriz, unsigned short int* Dimension)
 {
+    for (short int Fila=0; Fila<(*Dimension); Fila++)
+    {
+        delete Matriz[Fila];
+    }
 
+    delete Matriz;
 }
 
 short unsigned int TransformarFila(unsigned short int Fila, unsigned short int Columna, unsigned short int* Dimension, unsigned short int* CantRotaciones)
@@ -82,19 +122,42 @@ short unsigned int TransformarColumna(unsigned short int Fila, unsigned short in
     }
 }
 
-unsigned short int ObtenerValorCasilla(unsigned short int Fila, unsigned short int Columna, unsigned short int* Dimension, char Centro)
+unsigned short int ObtenerValorCasilla(unsigned short int Fila, unsigned short int Columna, unsigned short int* Dimension)
 {
-    if (Centro == '-')
+    if (PosRespectoCentro(&Fila, &Columna, Dimension) == '-')
     {
         return ( (Columna) + ((Fila-1)*(*Dimension)) );
     }
-    else if (Centro == '+')
+    else if (PosRespectoCentro(&Fila, &Columna, Dimension) == '+')
     {
         return ( (Columna) + ((Fila-1)*(*Dimension)) ) -1;
     }
 }
 
-/*void ImprimirMatriz(unsigned short int** Matriz, unsigned short int* Dimension)
+char PosRespectoCentro(unsigned short int* Fila, unsigned short int* Columna, unsigned short int* Dimension)
+{
+    if (*Fila<((*Dimension)/2)+1)
+    {
+        return '-';
+    }
+    else if (*Fila>((*Dimension)/2)+1)
+    {
+        return '+';
+    }
+    else if (*Fila==((*Dimension)/2)+1)
+    {
+        if (*Columna<((*Dimension)/2)+1)
+        {
+            return '-';
+        }
+        else if (*Columna>((*Dimension)/2)+1)
+        {
+            return '+';
+        }
+    }
+}
+
+void ImprimirMatriz(unsigned short int** Matriz, unsigned short int* Dimension)
 {
     for (short int Fila=0; Fila<(*Dimension); Fila++)
     {
@@ -105,4 +168,4 @@ unsigned short int ObtenerValorCasilla(unsigned short int Fila, unsigned short i
 
         std::cout << "\n";
     }
-}*/
+}
--- Matriz.h
+++ Matriz.h
@@ -2,11 +2,13 @@
 #define MATRIZ_H
 
 unsigned short int** CrearMatriz(unsigned short int* Dimension);
-void RotarMatriz(unsigned short int** Matriz, unsigned short int* Dimension, short int CantRotaciones);
+void RotarMatriz(unsigned short int** Matriz, unsigned short int* Dimension,unsigned short int CantRotaciones);
 unsigned short int ObtenerEstadoRotacion(unsigned short int** Matriz, unsigned short int* Dimension);
 void EliminarMatriz(unsigned short int** Matriz, unsigned short int* Dimension);
 short unsigned int TransformarFila(unsigned short int Fila, unsigned short int Columna, unsigned short int* Dimension, unsigned short int* CantRotaciones);
 short unsigned int TransformarColumna(unsigned short int Fila, unsigned short int Columna, unsigned short int* Dimension, unsigned short int* CantRotaciones);
-unsigned short int ObtenerValorCasilla(unsigned short int Fila, unsigned short int Columna, unsigned short int* Dimension, char Centro);
+unsigned short int ObtenerValorCasilla(unsigned short int Fila, unsigned short int Columna, unsigned short int* Dimension);
+char PosRespectoCentro(unsigned short int* Fila, unsigned short int* Columna, unsigned short int* Dimension);
+void ImprimirMatriz(unsigned short int** Matriz, unsigned short int* Dimension);
 
 #endif // MATRIZ_H
