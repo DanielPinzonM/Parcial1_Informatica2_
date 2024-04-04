3ec7199a98d27585df77aa090ccc1a0dba4d165b
DanielPinzonM <daniele.pinzon@udea.edu.co>
2024-04-03 23:08:15 -0500

    Se modifica para probar y verificar el correcto funcionamiento de las nuevas funciones agregadas al modulo

--- main.cpp
+++ main.cpp
@@ -3,19 +3,21 @@
 
 int main()
 {
-    unsigned short int Dimension = 7;
+    unsigned short int Dimension = 5;
 
     unsigned short int** Matriz = CrearMatriz(&Dimension);
 
-    for (short int Fila=0; Fila<(Dimension); Fila++)
-    {
-        for (short int Columna=0; Columna<(Dimension); Columna++)
-        {
-            std::cout << Matriz[Fila][Columna] << " ";
-        }
+    ImprimirMatriz(Matriz, &Dimension);
 
-        std::cout << "\n";
-    }
+    std::cout << "\n\n";
+
+    RotarMatriz(Matriz, &Dimension, 2);
+
+    ImprimirMatriz(Matriz, &Dimension);
+
+    std::cout << "\nEstado de rotacion = " << ObtenerEstadoRotacion(Matriz, &Dimension);
+
+    EliminarMatriz(Matriz, &Dimension);
 
     return 0;
 }
