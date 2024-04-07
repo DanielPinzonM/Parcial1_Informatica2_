7643db409686acf077b4ea8f46d4e63f80d45517
NicolasVZ11 <142182818+NicolasVZ11@users.noreply.github.com>
2024-04-07 18:57:14 -0500

    Se corrigen errores de las funciones del modulo Comprobaciones.

--- FuncionesComprobacion.cpp
+++ FuncionesComprobacion.cpp
@@ -1,85 +1,92 @@
 #include "Matriz.h"
 #include "Cerradura.h"
-/*
-void ComprobacionDeCerradura(unsigned short int ***Cerradura,unsigned short int Fila, unsigned short Columna,short int *Arreglo_Comparaciones,int CantidadMatrices)
-{
-    unsigned short int Diferencia_entre_dimensiones, FilaOriginal, ColumnaOriginal;
-    unsigned short int Dimension_Matriz_Primera, Dimension_Matriz_Segunda;
 
-    for(int i=CantidadMatrices-2;  i >= 0; i--)
+void CambiosCerradura(unsigned short int ***Cerradura,unsigned short int Fila, unsigned short Columna,short int *Arreglo_Comparaciones,int CantidadComparaciones,unsigned short int DimensionOriginal)
+{
+    unsigned short int FilaOriginal, ColumnaOriginal;
+    unsigned short int Dimension_Matriz_Primera, Dimension_Matriz_Segunda,DiferenciaM1,DiferenciaM2;
+
+    for(int i=CantidadComparaciones-1;  i >= 0; i--)
     {
         Dimension_Matriz_Primera=ObtenerDimension(Cerradura[i]);
         Dimension_Matriz_Segunda=ObtenerDimension(Cerradura[1+i]);
-        FilaOriginal=ObtenerFilaOriginal(Fila,Dimension_Matriz_Primera);
-        ColumnaOriginal=ObtenerColumnaOriginal(Columna,Dimension_Matriz_Primera);
+        DiferenciaM1=(ObtenerDimension(Cerradura[i])-(DimensionOriginal))/2;
+        DiferenciaM2=(ObtenerDimension(Cerradura[i+1])-(DimensionOriginal))/2;
+        FilaOriginal=ObtenerFilaOriginal(Cerradura[i][Fila-1+DiferenciaM1][Columna-1+DiferenciaM1],Dimension_Matriz_Primera);
+        ColumnaOriginal=ObtenerColumnaOriginal(Cerradura[i][Fila-1+DiferenciaM1][Columna-1+DiferenciaM1],Dimension_Matriz_Primera);
 
-        if (Arreglo_Comparaciones[(CantidadMatrices-1)-1-i] == 1)
+        if (Arreglo_Comparaciones[i] == 1)
         {
-            if ( FilaOriginal <= (Dimension_Matriz_Primera/2 +1) && (ColumnaOriginal>Dimension_Matriz_Primera))
+            if(Cerradura[i][Fila-1+DiferenciaM1][Columna-1+DiferenciaM1] <= Cerradura[i+1][Fila-1+DiferenciaM2][Columna-1+DiferenciaM2])
             {
-                EliminarMatriz(Cerradura[i]);
-                if(Dimension_Matriz_Primera > Dimension_Matriz_Segunda)
+                if ( FilaOriginal <= (Dimension_Matriz_Primera/2 ) && (ColumnaOriginal < Dimension_Matriz_Primera))
                 {
-                    Cerradura[i]=CrearMatriz(Dimension_Matriz_Segunda);
-                }
-                else
-                {
-                    Cerradura[i+1]=CrearMatriz(Dimension_Matriz_Primera);
-                }
-            }
-            else if	( FilaOriginal>(Dimension_Matriz_Primera/2) && ColumnaOriginal<=(Dimension_Matriz_Primera/2 +1))
-            {
-                IgualarRotacion(Cerradura[i],Cerradura[i+1]);
-                RotarMatriz(Cerradura[i+1], 2);
-            }
-            else if (ColumnaOriginal == Dimension_Matriz_Primera && FilaOriginal>(Dimension_Matriz_Primera/2) or ColumnaOriginal>(Dimension_Matriz_Primera/2 +1) && FilaOriginal >(Dimension_Matriz_Primera/2))
-            {
-                IgualarRotacion(Cerradura[i],Cerradura[i+1]);
-                RotarMatriz(Cerradura[i+1], 3);
-            }
 
+                    CambiarMatriz(Cerradura[i],ObtenerDimension(Cerradura[i])+2);
+                }
+                else if	( FilaOriginal > (Dimension_Matriz_Primera/2) && ColumnaOriginal<=((Dimension_Matriz_Primera/2)+1))
+                {
+                    RotarMatriz(Cerradura[i+1],ObtenerEstadoRotacion(Cerradura[i])+1);
+                }
+                else if ( (ColumnaOriginal == Dimension_Matriz_Primera && FilaOriginal>(Dimension_Matriz_Primera/2) ) || (ColumnaOriginal>( (Dimension_Matriz_Primera/2) +1) && FilaOriginal >(Dimension_Matriz_Primera/2)))
+                {
+                    RotarMatriz(Cerradura[i+1],ObtenerEstadoRotacion(Cerradura[i])+3);
+                }
+            }
         }
 
-        else if (Arreglo_Comparaciones[(CantidadMatrices-1)-1-i] == -1)
+        else if (Arreglo_Comparaciones[i] == -1)
         {
-
-        }
-
-        else if (Arreglo_Comparaciones[(CantidadMatrices-1)-1-i] == 0)
-        {
-
-            if(Dimension_Matriz_Primera != Dimension_Matriz_Segunda) // si la dimenension no es igual retorna falso
+            if(Cerradura[i][Fila-1+DiferenciaM1][Columna-1+DiferenciaM1] >= Cerradura[i+1][Fila-1+DiferenciaM2][Columna-1+DiferenciaM2])
             {
-                if(Dimension_Matriz_Primera > Dimension_Matriz_Segunda)
+                if ( FilaOriginal > ((Dimension_Matriz_Primera/2)+1 ) && (ColumnaOriginal > 1 ))
                 {
-                    EliminarMatriz(Cerradura[i]);
-                    Cerradura[i]=CrearMatriz(Dimension_Matriz_Segunda);
+                    CambiarMatriz(Cerradura[i+1],ObtenerDimension(Cerradura[i+1])+2);
                 }
-                else
+                else if	( FilaOriginal <= ( (Dimension_Matriz_Primera/2)+1 ) && ColumnaOriginal> ((Dimension_Matriz_Primera/2)))
                 {
-                    EliminarMatriz(Cerradura[i+1]);
-                    Cerradura[i+1]=CrearMatriz(Dimension_Matriz_Primera);
+                    RotarMatriz(Cerradura[i+1],ObtenerEstadoRotacion(Cerradura[i])+1);
                 }
-                IgualarRotacion(Cerradura[i],Cerradura[i+1]);
-
+                else if ( (ColumnaOriginal == 1 && FilaOriginal>((Dimension_Matriz_Primera/2)+1 ) ) || (ColumnaOriginal<= (Dimension_Matriz_Primera/2)  && FilaOriginal <= ((Dimension_Matriz_Primera/2)+1 )))
+                {
+                    RotarMatriz(Cerradura[i+1],ObtenerEstadoRotacion(Cerradura[i])+3);
+                }
             }
-            else if (ObtenerEstadoRotacion(Cerradura[i]) != ObtenerEstadoRotacion(Cerradura[i+1]))
+        }
+        else if (Arreglo_Comparaciones[i] == 0)
+        {
+            if(Cerradura[i][Fila-1+DiferenciaM1][Columna-1+DiferenciaM1] != Cerradura[i+1][Fila-1+DiferenciaM2][Columna-1+DiferenciaM2])
             {
-                IgualarRotacion(Cerradura[i],Cerradura[i+1]);
+                if(Dimension_Matriz_Primera != Dimension_Matriz_Segunda) // si la dimenension no es igual retorna falso
+                {
+                    if(Dimension_Matriz_Primera > Dimension_Matriz_Segunda)
+                    {
+                        CambiarMatriz(Cerradura[i+1],ObtenerDimension(Cerradura[i]));
+                    }
+                    else
+                    {
+                        CambiarMatriz(Cerradura[i],ObtenerDimension(Cerradura[i+1]));
+                    }
+                    IgualarRotacion(Cerradura[i],Cerradura[i+1]);
+
+                }
+                else if (ObtenerEstadoRotacion(Cerradura[i]) != ObtenerEstadoRotacion(Cerradura[i+1]))
+                {
+                    IgualarRotacion(Cerradura[i],Cerradura[i+1]);
+                }
             }
         }
     }
 
-//sale del bucle;
+    //sale del bucle;
 }
-*/
 
-bool Abrecerradura(unsigned short int ***Cerradura,unsigned short int Fila, unsigned short Columna,short int *Arreglo_Comparaciones,int CantidadMatrices, unsigned short int DimensionOriginal )
+bool Abrecerradura(unsigned short int ***Cerradura,unsigned short int Fila, unsigned short Columna,short int *Arreglo_Comparaciones,int CantidadComparaciones, unsigned short int DimensionOriginal )
 {
     unsigned short int DiferenciaM1, DiferenciaM2;
 
 
-    for(int i=0;  i < CantidadMatrices; i++)
+    for(int i=0;  i < CantidadComparaciones; i++)
     {
         DiferenciaM1=(ObtenerDimension(Cerradura[i])-(DimensionOriginal))/2;
         DiferenciaM2=(ObtenerDimension(Cerradura[i+1])-(DimensionOriginal))/2;
--- FuncionesComprobacion.h
+++ FuncionesComprobacion.h
@@ -1,2 +1,6 @@
-bool Abrecerradura( unsigned short int ***Cerradura,unsigned short int Fila, unsigned short Columna,short int *Arreglo_Comparaciones,int CantidadMatrices,unsigned short int DimensionOriginal);
-bool ComprobacionDeCerradura(unsigned short int ***Cerradura,unsigned short int Fila, unsigned short Columna,short int *Arreglo_Comparaciones,int CantidadMatrices);
+#ifndef FUNCIONESCOMPROBACION_H
+#define FUNCIONESCOMPROBACION_H
+
+#endif // FUNCIONESCOMPROBACION_H
+bool Abrecerradura( unsigned short int ***Cerradura,unsigned short int Fila, unsigned short Columna,short int *Arreglo_Comparaciones,int CantidadComprobaciones,unsigned short int DimensionOriginal);
+bool CambiosCerradura(unsigned short int ***Cerradura,unsigned short int Fila, unsigned short Columna,short int *Arreglo_Comparaciones,int CantidadComparaciones,unsigned short int DimensionOriginal);
